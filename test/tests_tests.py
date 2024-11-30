a3ab3f43918436ec8de544446ce61e605461b1f3
NISHINOKIRI <v.garanin@gost.team>
2024-11-30 20:38:17 +0300

    rework loggeer and another changes

--- test/tests_tests.py
+++ test/tests_tests.py
@@ -2,24 +2,17 @@ import pytest
 from selenium.webdriver.common.by import By
 from utils.logger import Logger
 
+# Конфигурируем логер
+logger = Logger().get_logger()  # Получаем логер
+i = logger.info # Упрощаем код
+
 @pytest.mark.usefixtures("browser")
 def test_google_logo(browser):
-    logger = Logger().get_logger()  # Получаем логгер
-    logger.info("Запуск теста Google")
-
-    try:
-        logger.info("Открываем страницу Google")
-        browser.get('https://www.google.com/')
-
-        element = browser.find_element(By.XPATH, '//input[@name="q"]')  # Более стабильный XPATH
-        logger.info(f"Полученное значение элемента: '{element.get_attribute('value')}'")
-
-        assert 'Google' in element.get_attribute('value'), f"Ожидалось 'Google', но получено '{element.get_attribute('value')}'"
-        logger.info("Тест пройден успешно")
-
-    except AssertionError as e:
-        logger.error(f"Тест не пройден: {e}")
-    except Exception as e:
-        logger.exception("Произошла ошибка")
-    finally:
-        logger.info("Тест завершен")
+    test_name = 'Google check'
+    i("Запуск теста")
+    i("Открытие страницы Google")
+    browser.get('https://www.google.com/')
+    element = browser.find_element(By.XPATH, '/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')  # Более стабильный XPATH
+    i(f"Полученное значение элемента: '{element.get_attribute('value')}'")
+    assert 'Поиск в Google' == element.get_attribute('value'), f"Ожидалось 'Поиск в Google', но получено '{element.get_attribute('value')}'"
+    i(f"Тест {test_name} завершен")
